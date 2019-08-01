7d9e14f63cf227f6bdd5eea829fea23e15f29d4f
CyberWendy123 <xiaowenx@alumni.wfu.edu>
2019-08-01 13:55:51 -0400

    fist commit

---
+++ freqNum.cpp
@@ -0,0 +1,11 @@
+/* this program is trying to find the most freq element in an array
+*   created on August 1, 2019
+*   by Wendy Xiao
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  return 0;
+}
