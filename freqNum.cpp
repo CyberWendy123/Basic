95de5079258fa3c7568ee5b14ed61f868d77a9a4
CyberWendy123 <xiaowenx@alumni.wfu.edu>
2019-08-05 19:31:56 -0400

    aug 5

--- freqNum.cpp
+++ freqNum.cpp
@@ -3,9 +3,51 @@
 *   by Wendy Xiao
 */
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+//the function to find the frequency
+int mostFreq(int arr[], int arraySize){
+
+  sort(arr, arr + arraySize); //Sort the array
+
+  int maxCount = 0;
+  int res = arr[0];
+  int currCount = 1;
+  for(int i = 1; i < arraySize; i++){
+    if(arr[i] == arr[i-1])
+      currCount++;
+    else{
+      if(currCount > maxCount){
+        maxCount = currCount;
+        res = arr[i-1];
+      }
+      currCount = 1;
+    }
+  }
+
+  //if last elemnt is most freq
+  if(currCount > maxCount){
+    maxCount = currCount;
+    res = arr[arraySize-1];
+  }
+
+  return res;
+}
+
+//int mostFreqUnsorted();
+
 int main(){
+  //first create a random array list from 0 to 24
+  srand(time(NULL));
+
+  int rNum[10]; //= rand() % 100;
+  int arraySize = 30;
+  for(int i = 0; i < arraySize; i++)
+    rNum[i] = rand() % 25;
+
+  //then find frequency
+  cout << mostFreq(rNum, arraySize) << endl;
 
   return 0;
 }
---
+++ ten.cpp
@@ -0,0 +1,16 @@
+/*
+* ten.cpp 
+*	created on August 5, 2029 
+* 	by Wendy Xiao 
+* 	goal: find paris in an integer array whose sum is equal to 10 
+*	(bonus: do it in linear time)
+*/ 
+#include <iostream> 
+using namespace std; 
+
+int main(){
+
+	int arr[] = {-20, -5, 7, -12, 11, 25, 23, 2, 4, 5, 1, 16, 8, 9, -9, -23, -3, -13, -24, 3, 5, -18, 20, -12, -25}; 
+
+	return 0; 
+}
